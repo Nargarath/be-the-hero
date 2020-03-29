a65961646cf4e2252ea1b5859e2c741974087db5
Mateus Cordeiro <mateusmcordeiro@hotmail.com>
2020-03-29 02:09:48 -0300

    logon and profile pages implements api

--- frontend/src/pages/Logon/index.tsx
+++ frontend/src/pages/Logon/index.tsx
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles.scss';
 import { FiLogIn } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import logoImg from '../../assets/svg/logo.svg';
 import heroesImg from '../../assets/svg/heroes.svg';
+import api from "../../services/api";
+import { useHistory } from 'react-router-dom';
 
 export default function Logon() {
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(e) {
+        e.preventDefault();
+
+        try {
+            const response  = await api.post('sessions', { id });
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName',response.data.name);
+            history.push('/profile');
+        } catch (err) {
+            alert('Falha no login, tente novamente.');
+        }
+    }
+
     return(
      <div className="logon-container">
          <section className="form">
             <img src={logoImg} alt="Be The Hero" />
-             <form action="">
+             <form onSubmit={handleLogin}>
                  <h1>Faça seu logon</h1>
 
-                 <input type="text" placeholder="Sua ID"/>
+                 <input
+                     type="text"
+                     placeholder="Sua ID"
+                     value={id}
+                     onChange={ e => setId(e.target.value)}
+                 />
                  <button className="button" type="submit">Entrar</button>
                  <Link to="/register" className="back-link">
                      <FiLogIn size={20} color="#E02041"/>
--- frontend/src/pages/Profile/index.tsx
+++ frontend/src/pages/Profile/index.tsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './styles.scss';
 import { FiPower, FiPlus } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import logoImg from '../../assets/svg/logo.svg';
 import IncidentBox from "../../components/IncidentBox";
+import api from "../../services/api";
 
 export default function Profile() {
+    const [incidents, setIncidents] = useState<any[]>([])
+;
+    const ongName = localStorage.getItem('ongName') || 'error';
+    const ongId = localStorage.getItem('ongId') || 'error';
+
+    async function boot() {
+        const response = await api.get('profile', {
+            headers: {
+                Authorization: ongId
+            }
+        });
+        setIncidents(response.data);
+    }
+
+    useEffect(
+        () => {
+            boot()
+        },
+        []
+    );
+
     return(
      <div className="profile-container">
          <nav>
              <div className="box-left">
                  <img src={logoImg} alt={'Be the Hero'}/>
-                 <p>Bem vinda,  APAD</p>
+                 <p>Bem vinda,  {ongName}</p>
              </div>
              <div className="box-right">
                  <Link to="/register-incident" className="button" id="new-incident" >
@@ -23,32 +45,20 @@ export default function Profile() {
              </div>
          </nav>
          <section className="profile-section">
-             <h1>Casos cadastrados</h1>
+             <h1>{ incidents.length > 0 ? 'Casos cadastrados' : 'Nenhum caso cadastrado' }</h1>
              <div className="profile-content-body">
-                <IncidentBox
-                    id={11}
-                    title={'Cadelinha atropelada'}
-                    description={'A cadelinha Jolie foi atropelada por um carro no bairro Santana e teve que passar por uma cirurgia às pressas.'}
-                    value={120.00}
-                />
-                 <IncidentBox
-                     id={12}
-                     title={'Cadelinha atropelada'}
-                     description={'A cadelinha Jolie foi atropelada por um carro no bairro Santana e teve que passar por uma cirurgia às pressas.'}
-                     value={120.00}
-                 />
-                 <IncidentBox
-                     id={13}
-                     title={'Cadelinha atropelada'}
-                     description={'A cadelinha Jolie foi atropelada por um carro no bairro Santana e teve que passar por uma cirurgia às pressas.'}
-                     value={120.00}
-                 />
-                 <IncidentBox
-                     id={14}
-                     title={'Cadelinha atropelada'}
-                     description={'A cadelinha Jolie foi atropelada por um carro no bairro Santana e teve que passar por uma cirurgia às pressas.'}
-                     value={120.00}
-                 />
+                 {
+                     incidents.map(
+                         incident => (
+                             <IncidentBox
+                                 id={incident.id}
+                                 title={incident.title}
+                                 description={incident.description}
+                                 value={incident.value}
+                             />
+                         )
+                     )
+                 }
              </div>
          </section>
      </div>
